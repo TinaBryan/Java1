8b4d5ac0061df7801fce9540d7a411898fffde3d
TinaBryan <tdmak63@gmail.com>
2017-11-08 00:50:50 -0500

    created a String variable that prints firstName to the console

--- introductions.java
+++ introductions.java
@@ -3,7 +3,9 @@ import java.io.Console;
 public class Introductions {
 	public static void main(String[] args) {
 		Console console = System.console();
+		String firstName = "Tina";
 		// Welcome to the Introductions program! Your code goes below here
-		console.printf("Hello, my name is Tina!");
+		console.printf("Hello, my name is %s \n", firstName);
+		console.printf("%s is learning how to write Java\n", firstName);
 	}
 }
